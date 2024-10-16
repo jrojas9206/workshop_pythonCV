ec2b5c0c36711cb4fb7e2ca87c2ce9dd7ebb4d1d
Juan Pablo Rojas Bustos <jrojas9206@gmail.com>
2024-10-16 13:41:07 +0300

    Updating text practice

---
+++ section_1_pythonIntroduction/practiceExcersice/blinkMessage.py
@@ -0,0 +1,29 @@
+def blinkText(msg:str, dtime:float) -> None:
+    '''
+        Make blink a message surrounded by some asterisk
+        e.g:
+        t0:
+            **********
+            ** HOLA **
+            **********
+        t1:
+            **********
+            **      **
+            **********
+
+        :Args:
+            - msg: str, Word or phrase to show 
+            - dtime: float, Blinking period on off/on 
+    '''
+
+    """
+        Your code goes here
+    """
+    return 0 
+
+def main():
+    blinkText()
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ section_1_pythonIntroduction/practiceExcersice/dissapearingText.py
@@ -0,0 +1,26 @@
+def dissapearingText(msg:str, speed:float=0.5) -> None:
+    '''
+        Make dissapear a word of phrase chracter by character
+        e.g:
+
+        T0:
+           Hola Mundo!
+        T1:
+           Ola Mundo!
+        T2:
+            la Mundo!
+        
+        :Args:
+          - msg: str, Message to show
+          - speed: float, Speed in which the text have to dessapear. Default: 0.5
+    '''
+    """
+        Your code goes here!!
+    """
+
+def main():
+    dissapearingText()
+    return 0
+
+if __name__ == '__main__':
+    main()
