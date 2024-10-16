fb5d1c4f350cc60f3fcc6b650c9ff18e6501f4b1
Juan Pablo Rojas Bustos <jrojas9206@gmail.com>
2024-10-16 13:10:05 +0300

    update variablesAndDataStructures

--- section_1_pythonIntroduction/variablesAndDataStructures/main.py
+++ section_1_pythonIntroduction/variablesAndDataStructures/main.py
@@ -1,3 +1,12 @@
+#########
+# INTRO #
+#########
+
+'''
+ In the following script you will find some basic operations 
+ that can be applied to variable and datastructures
+'''
+
 # Testing the float variables 
 print('-> Testing floats')
 try:
@@ -11,6 +20,10 @@ try:
     print(' ', value)
 except OverflowError:
     print(' [ERROR] the value can not be keep it in memory')
+
+#########
+# Lists #
+#########
 
 # Example of list creation 
 L0 = list()
@@ -50,4 +63,34 @@ print('  -> Reversing the order of L0 - L0.reverse()')
 L1.reverse()
 print('  ', L1)
 
-# Tuples 
+##########
+# Tuples #
+##########
+
+print('\Tuple definitions')
+
+T0 = tuple()
+T1 = (1, 2, 3)
+T2 = (i for i in range(4)) # It will return the pointer of the object 
+T21 = tuple(i for i in range(4)) # Will return the object - after conversion 
+T3 = (L1, [4,5,6], 7)
+
+try:
+    T1[0] = 4
+except TypeError:
+    print('[ERROR] It is not possible to modify a tuple they are immutable')
+
+print('\n-> Creating a tuple from 1 till 100')
+T0to100 = tuple(i for i in range(1,101))
+print('  ', T0to100)
+
+print('\n-> Selecting the second position of the tuple T3')
+print('T3[1]= ', T3[1])
+
+print('-> Counting elements in a tuple, T0.count(VALUE)')
+T0 = [1,2,2,3,4,4]
+print(' -> Tuple T0 = ', T0)
+# How many 4 are in the tuple T0
+foursInT0 = T0.count(4)
+
+print(f' -> There are {foursInT0} Fours in T0')
